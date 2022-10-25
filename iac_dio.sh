105ba0b49c14845f14d9a8ab4714a40d07764e07
giomartins12 <giomartins@gmail.com>
2022-10-25 12:39:24 -0300

    Correção do script

--- iac_dio.sh
+++ iac_dio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Criando diretórios..."
-
+echo -e "Criando diretórios...\n"
+sleep 2
 mkdir /publico
 echo "Diretorio publico criado!"
 sleep 3
@@ -12,12 +12,12 @@ mkdir /ven
 echo "Diretorio ven criado!"
 sleep 3
 mkdir /sec
-echo "Diretorio sec criado!"
-sleep 3
+echo -e "Diretorio sec criado!\n"
+sleep 2
 
 
-echo "Criando grupos de usuários..."
-
+echo -e "Criando grupos de usuários...\n"
+sleep 2
 groupadd GRP_ADM
 echo "Grupo ADM criado!"
 sleep 3
@@ -26,12 +26,12 @@ echo "Grupo VEN criado!"
 sleep 3
 groupadd GRP_SEC
 echo "Grupo SEC criado!"
-sleep 3
-
-echo "Criando usuários..."
 sleep 2
-echo "Criando usuários do grupo ADM... "
 
+echo -e "Criando usuários...\n"
+sleep 2
+echo -e "Criando usuários do grupo ADM...\n "
+sleep 2
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 echo "Usuário Carlos criado!"
 sleep 3
@@ -42,8 +42,8 @@ useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 echo "Usuário João criado!"
 sleep 3
 
-echo "Criando usuários do grupo VEN... "
-
+echo -e "Criando usuários do grupo VEN...\n "
+sleep 2
 useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 echo "Usuário Debora criado!"
 sleep 3
@@ -54,8 +54,8 @@ useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 echo "Usuário Roberto criado!"
 sleep 3
 
-echo "Criando usuários do grupo SEC... "
-
+echo -e "Criando usuários do grupo SEC...\n "
+sleep 2
 useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 echo "Usuário Josefina criado!"
 sleep 3
@@ -67,20 +67,25 @@ echo "Usuário Rogerio criado!"
 sleep 3
 
 echo "Especificando permissões dos diretórios...."
-
+echo -e "\n"
+sleep 2
 chown root:GRP_ADM /adm
 chmod 770 /adm
+ls -l | grep adm
 cat /etc/group |grep GRP_ADM
 sleep 4
 chown root:GRP_VEN /ven
 chmod 770 /ven
+ls -l | grep ven
 cat /etc/group |grep GRP_VEN
 sleep 4
 chown root:GRP_SEC /sec
 chmod 770 /sec
+ls -l | grep sec
 cat /etc/group |grep GRP_SEC
 sleep 4
 
 chmod 777 /publico
+ls -l | grep publico
 
 echo "Executado com sucesso!"
